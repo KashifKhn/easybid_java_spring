63ace2a9b8d28d9b15cec1f08d2e5059d2031eb0
KashifKhn <kashifkhnx04@gmail.com>
2024-12-01 18:29:55 +0500

    feat: add the user repository

--- src/main/java/com/easybid/Users/UserRepository.java
+++ src/main/java/com/easybid/Users/UserRepository.java
@@ -1,11 +1,22 @@
 package com.easybid.Users;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends ListPagingAndSortingRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+  Optional<UserEntity> findByEmail(String email);
+
+  @Query("SELECT u FROM UserEntity u WHERE u.deletedAt IS NULL")
+  List<UserEntity> findAllActiveUsers();
+
+  @Query("SELECT u FROM UserEntity u WHERE u.id = :userId AND u.deletedAt IS NULL")
+  Optional<UserEntity> findActiveUserById(UUID userId);
 
 }
