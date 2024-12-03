a82a53f35079d1f26017a6052dc22821c1997632
KashifKhn <kashifkhnx04@gmail.com>
2024-12-03 18:32:51 +0500

    refactor: add the max size validatin to user create dto

--- src/main/java/com/easybid/user/dto/CreateUserDTO.java
+++ src/main/java/com/easybid/user/dto/CreateUserDTO.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,22 +19,27 @@ public class CreateUserDTO {
 
   @NotBlank(message = "Name must not be null or blank")
   @Schema(description = "Name of the user", example = "John Doe")
+  @Size(max = 255, min = 1)
   private String name;
 
   @Email(message = "Invalid email format")
   @NotBlank(message = "Email must not be null or blank")
   @Schema(description = "Email of the user", example = "john.doe@example.com")
+  @Size(max = 255, min = 1)
   private String email;
 
   @NotBlank(message = "Password must not be null or blank")
   @Schema(description = "Password of the user", example = "securePassword123")
+  @Size(max = 255, min = 1)
   private String password;
 
   @NotBlank(message = "Address must not be null or blank")
   @Schema(description = "Address of the user", example = "123 Main St, Cityville")
+  @Size(max = 255, min = 1)
   private String address;
 
   @Schema(description = "Phone number of the user", example = "123-456-7890")
+  @Size(max = 255)
   private String phoneNumber;
 
   @NotNull(message = "Role must not be null")
