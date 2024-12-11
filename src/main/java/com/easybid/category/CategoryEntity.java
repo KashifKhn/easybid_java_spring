a645f2332510d364dcd2d0fefadc147de9f297ab
KashifKhn <kashifkhnx04@gmail.com>
2024-12-11 11:41:45 +0500

    refactor: add the one to many relation b/w the items and category

--- src/main/java/com/easybid/category/CategoryEntity.java
+++ src/main/java/com/easybid/category/CategoryEntity.java
@@ -1,11 +1,15 @@
 package com.easybid.category;
 
+import java.util.Set;
+
 import com.easybid.category.dto.UpdateCategoryDTO;
 import com.easybid.common.BaseEntity;
+import com.easybid.item.ItemEntity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -29,6 +33,9 @@ public class CategoryEntity extends BaseEntity {
 
   @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT true")
   private Boolean isActive;
+
+  @OneToMany(mappedBy = "category")
+  private Set<ItemEntity> items;
 
   public void updateFromDTO(UpdateCategoryDTO dto) {
     if (dto.getName() != null && !dto.getName().isBlank()) {
