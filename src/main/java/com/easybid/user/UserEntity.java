336c640b3d997de2c908678f6c2d32cde85541fe
KashifKhn <kashifkhnx04@gmail.com>
2024-12-11 11:41:28 +0500

    refactor: add the one to many relation b/w the items and users

--- src/main/java/com/easybid/user/UserEntity.java
+++ src/main/java/com/easybid/user/UserEntity.java
@@ -1,15 +1,19 @@
 package com.easybid.user;
 
 import com.easybid.user.dto.UpdateUserDTO;
+
+import java.util.Set;
+
 import com.easybid.auth.PasswordHasher;
 import com.easybid.common.BaseEntity;
 import com.easybid.enums.UsersRole;
-
+import com.easybid.item.ItemEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
@@ -45,6 +49,9 @@ public class UserEntity extends BaseEntity {
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
   private UsersRole role;
+
+  @OneToMany(mappedBy = "user")
+  private Set<ItemEntity> items;
 
   public void updateFromDTO(UpdateUserDTO dto, PasswordHasher passwordHasher) {
     if (dto.getName() != null && !dto.getName().isBlank()) {
