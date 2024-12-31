3fe8016d27ed24c0af33e08fe9750b2bb9370261
KashifKhn <kashifkhnx04@gmail.com>
2024-12-31 05:58:24 +0500

    feat: add custom query

--- src/main/java/com/easybid/auction/AuctionRepository.java
+++ src/main/java/com/easybid/auction/AuctionRepository.java
@@ -5,12 +5,22 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<AuctionEntity, UUID> {
 
+  List<AuctionEntity> findByItemId(UUID itemId);
+
   List<AuctionEntity> findByDeletedAtIsNull();
+
+  @Query("SELECT a FROM AuctionEntity a WHERE a.item.user.id = :userId AND a.deletedAt IS NULL")
+  List<AuctionEntity> findByItemUserId(@Param("userId") UUID userId);
+
+  @Query("SELECT a FROM AuctionEntity a WHERE a.item.id = :itemId AND a.item.user.id = :userId AND a.deletedAt IS NULL")
+  List<AuctionEntity> findByItemIdAndUserId(@Param("itemId") UUID itemId, @Param("userId") UUID userId);
 
   Optional<AuctionEntity> findByIdAndDeletedAtIsNull(UUID auctionId);
 
