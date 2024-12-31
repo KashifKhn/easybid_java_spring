acc8b82f3c10362190ea44225e543e9fc81e78d8
KashifKhn <kashifkhnx04@gmail.com>
2024-12-31 05:58:08 +0500

    refactor: add higes bid and bid relationship

--- src/main/java/com/easybid/auction/AuctionEntity.java
+++ src/main/java/com/easybid/auction/AuctionEntity.java
@@ -1,13 +1,16 @@
 package com.easybid.auction;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import com.easybid.bid.BidEntity;
 import com.easybid.common.BaseEntity;
 import com.easybid.enums.AuctionStatus;
 import com.easybid.enums.AuctionType;
 import com.easybid.enums.IncrementType;
 import com.easybid.item.ItemEntity;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -15,6 +18,8 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
@@ -64,5 +69,14 @@ public class AuctionEntity extends BaseEntity {
 
   @Column()
   private Double incrementPercentage;
+
+  @OneToOne
+  @JoinColumn(name = "highest_bid_id")
+  @JsonManagedReference("highest-bid")
+  private BidEntity highestBid;
+
+  @OneToMany(mappedBy = "auction")
+  @JsonManagedReference("auction-bid")
+  private List<BidEntity> bids;
 
 }
