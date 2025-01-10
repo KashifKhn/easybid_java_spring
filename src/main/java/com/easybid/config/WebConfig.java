3874f0bf8b1d58e98cdf06e5c142c8daab877445
KashifKhn <kashifkhnx04@gmail.com>
2025-01-10 11:10:59 +0500

    feat: add auth config

--- src/main/java/com/easybid/config/WebConfig.java
+++ src/main/java/com/easybid/config/WebConfig.java
@@ -2,6 +2,7 @@
 package com.easybid.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/files/**")
+    registry
+        .addResourceHandler("/files/**")
         .addResourceLocations("file:" + Constants.STORAGE_DIR + "/");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true);
   }
 }
