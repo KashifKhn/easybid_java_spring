e41000e0b989d219731e6f3b4620383116b946d6
KashifKhn <kashifkhnx04@gmail.com>
2025-01-10 11:10:05 +0500

    refacotr: enable the secuitry

--- src/main/java/com/easybid/EasybidApplication.java
+++ src/main/java/com/easybid/EasybidApplication.java
@@ -2,9 +2,10 @@ package com.easybid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class }, scanBasePackages = "com.easybid")
+@SpringBootApplication(scanBasePackages = "com.easybid")
+@EnableScheduling
 public class EasybidApplication {
 
   public static void main(String[] args) {
